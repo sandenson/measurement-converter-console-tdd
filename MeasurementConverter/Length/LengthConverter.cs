82b737e8be5320f535f9aada67ffb4223c323fc5
sandenson <lgabrielsandes@gmail.com>
2023-11-11 17:27:34 -0300

    feat: Length converter created.

---
+++ MeasurementConverter/Length/LengthConverter.cs
@@ -0,0 +1,28 @@
+using MeasurementConverter.Length.Enums;
+
+namespace MeasurementConverter.Length
+{
+    public class LengthConverter
+    {
+        // meter
+        public static decimal MetricToMetric(decimal value, MetricLengthUnit fromUnit, MetricLengthUnit toUnit)
+        {
+            return 0M;
+        }
+
+        public static decimal ImperialToImperial(decimal value, ImperialLengthUnit fromUnit, ImperialLengthUnit toUnit)
+        {
+            return 0M;
+        }
+
+        public static decimal MetricToImperial(decimal value, MetricLengthUnit fromUnit, ImperialLengthUnit toUnit)
+        {
+            return 0M;
+        }
+
+        public static decimal ImperialToMetric(decimal value, ImperialLengthUnit fromUnit, MetricLengthUnit toUnit)
+        {
+            return 0M;
+        }
+    }
+}
--- MeasurementConverter/Program.cs
+++ MeasurementConverter/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿using MeasurementConverter.Length;
+using MeasurementConverter.Length.Enums;
+
+Console.WriteLine(LengthConverter.MetricToImperial(1, MetricLengthUnit.KILOMETER, ImperialLengthUnit.MILE));
+Console.WriteLine(LengthConverter.ImperialToMetric(1, ImperialLengthUnit.MILE, MetricLengthUnit.METER));
