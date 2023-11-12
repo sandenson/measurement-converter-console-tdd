b3d7aae8b09a85200b050a0139a905adb2105d24
sandenson <lgabrielsandes@gmail.com>
2023-11-12 01:04:03 -0300

    fix: Fix length unit values and length conversion test data.

--- MeasurementConverter.Test/LengthConverterTests.cs
+++ MeasurementConverter.Test/LengthConverterTests.cs
@@ -5,6 +5,11 @@ namespace MeasurementConverter.Test;
 
 public class UnitTest1
 {
+    private static decimal ToFixedDecimal(decimal value, int decimalPlaces)
+    {
+        return decimal.Parse(value.ToString("N" + decimalPlaces));
+    }
+
     [Theory]
     [InlineData(1, MetricLengthUnit.KILOMETER, MetricLengthUnit.METER, 1000)]
     [InlineData(1637, MetricLengthUnit.MILIMETER, MetricLengthUnit.METER, 1.637)]
@@ -17,12 +22,12 @@ public class UnitTest1
     {
         decimal result = LengthConverter.MetricToMetric(value, fromUnit, toUnit);
 
-        Assert.Equal(expected, result);
+        Assert.Equal(ToFixedDecimal(expected, 5), ToFixedDecimal(result, 5));
     }
 
     [Theory]
     [InlineData(6, ImperialLengthUnit.FOOT, ImperialLengthUnit.INCH, 72)]
-    [InlineData(7, ImperialLengthUnit.NAUTICAL_MILE, ImperialLengthUnit.MILE, 8.05546)]
+    [InlineData(7, ImperialLengthUnit.NAUTICAL_MILE, ImperialLengthUnit.MILE, 8.055456136164501)]
     [InlineData(338, ImperialLengthUnit.YARD, ImperialLengthUnit.FURLONG, 1.53636)]
     [InlineData(0.75, ImperialLengthUnit.CHAIN, ImperialLengthUnit.ROD, 3)]
     [InlineData(39, ImperialLengthUnit.LEAGUE, ImperialLengthUnit.FATHOM, 102960)]
@@ -32,29 +37,29 @@ public class UnitTest1
     {
         decimal result = LengthConverter.ImperialToImperial(value, fromUnit, toUnit);
 
-        Assert.Equal(expected, result);
+        Assert.Equal(ToFixedDecimal(expected, 5), ToFixedDecimal(result, 5));
     }
 
     [Theory]
     [InlineData(59, MetricLengthUnit.MICROMETER, ImperialLengthUnit.TWIP, 3.344881889763793)]
-    [InlineData(622, MetricLengthUnit.MILIMETER, ImperialLengthUnit.BARLEYCORN, 73.4646)]
+    [InlineData(622, MetricLengthUnit.MILIMETER, ImperialLengthUnit.BARLEYCORN, 73.46456692884)]
     [InlineData(1243, MetricLengthUnit.METER, ImperialLengthUnit.CHAIN, 61.78915)]
     [InlineData(66, MetricLengthUnit.KILOMETER, ImperialLengthUnit.MILE, 41.0105)]
-    [InlineData(0.000000985, MetricLengthUnit.PETAMETER, ImperialLengthUnit.NAUTICAL_MILE, 531.857645)]
+    [InlineData(0.000000985, MetricLengthUnit.TERAMETER, ImperialLengthUnit.NAUTICAL_MILE, 531.857451403888)]
     public void ShouldConvertMeasuresFromAMetricUnitToAnImperialUnitAndReturnTheExpectedResult(
         decimal value, MetricLengthUnit fromUnit, ImperialLengthUnit toUnit, decimal expected
     )
     {
         decimal result = LengthConverter.MetricToImperial(value, fromUnit, toUnit);
 
-        Assert.Equal(expected, result);
+        Assert.Equal(ToFixedDecimal(expected, 5), ToFixedDecimal(result, 5));
     }
 
     [Theory]
     [InlineData(59, ImperialLengthUnit.TWIP, MetricLengthUnit.MILIMETER, 1.0406944444444401)]
-    [InlineData(73, ImperialLengthUnit.BARLEYCORN, MetricLengthUnit.DECIMETER, 61.80666666691389)]
+    [InlineData(73, ImperialLengthUnit.BARLEYCORN, MetricLengthUnit.DECIMETER, 6.180666666691389)] // here
     [InlineData(61, ImperialLengthUnit.CHAIN, MetricLengthUnit.DECAMETER, 122.71248093555997)]
-    [InlineData(41, ImperialLengthUnit.MILE, MetricLengthUnit.KILOMETER, 65.9831244135951)]
+    [InlineData(41, ImperialLengthUnit.MILE, MetricLengthUnit.KILOMETER, 65.983104)]
     [InlineData(985, ImperialLengthUnit.NAUTICAL_MILE, MetricLengthUnit.MEGAMETER, 1.8242193359841616)]
     public void ShouldConvertMeasuresFromAnImperialUnitToAMetricUnitAndReturnTheExpectedResult(
         decimal value, ImperialLengthUnit fromUnit, MetricLengthUnit toUnit, decimal expected
@@ -62,6 +67,6 @@ public class UnitTest1
     {
         decimal result = LengthConverter.ImperialToMetric(value, fromUnit, toUnit);
 
-        Assert.Equal(expected, result);
+        Assert.Equal(ToFixedDecimal(expected, 5), ToFixedDecimal(result, 5));
     }
 }
