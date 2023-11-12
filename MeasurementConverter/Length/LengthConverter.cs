b7c4050bbc1733b5bc5fd6ce23b77905885f31a6
sandenson <lgabrielsandes@gmail.com>
2023-11-12 01:03:30 -0300

    feat: Implemented the length converter.

--- MeasurementConverter/Length/LengthConverter.cs
+++ MeasurementConverter/Length/LengthConverter.cs
@@ -7,22 +7,30 @@ namespace MeasurementConverter.Length
         // meter
         public static decimal MetricToMetric(decimal value, MetricLengthUnit fromUnit, MetricLengthUnit toUnit)
         {
-            return 0M;
+            return value * LengthUnitValues.Metric[fromUnit] / LengthUnitValues.Metric[toUnit];
         }
 
         public static decimal ImperialToImperial(decimal value, ImperialLengthUnit fromUnit, ImperialLengthUnit toUnit)
         {
-            return 0M;
+            return value * LengthUnitValues.Imperial[fromUnit] / LengthUnitValues.Imperial[toUnit];
         }
 
         public static decimal MetricToImperial(decimal value, MetricLengthUnit fromUnit, ImperialLengthUnit toUnit)
         {
-            return 0M;
+            return ImperialToImperial(
+                MetricToMetric(value, fromUnit, MetricLengthUnit.FOOT),
+                ImperialLengthUnit.FOOT,
+                toUnit
+            );
         }
 
         public static decimal ImperialToMetric(decimal value, ImperialLengthUnit fromUnit, MetricLengthUnit toUnit)
         {
-            return 0M;
+            return MetricToMetric(
+                ImperialToImperial(value, fromUnit, ImperialLengthUnit.METER),
+                MetricLengthUnit.METER,
+                toUnit
+            );
         }
     }
 }
